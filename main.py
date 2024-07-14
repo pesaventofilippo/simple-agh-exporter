f15548e65d59a04b2f8e07dfbb2c4d00ed8fa755
Filippo Pesavento <pesaventofilippo@gmail.com>
2024-07-14 15:45:20 +0200

    Respect the PROMETHEUS_PREFIX env variable

--- main.py
+++ main.py
@@ -12,35 +12,54 @@ agh = AGHClient(
 
 METRICS = {
     "dhcp": {
-        "enabled": prom.Gauge("dhcp_enabled", "DHCP server status"),
-        "active_leases": prom.Gauge("dhcp_active_leases", "Number of active DHCP leases"),
-        "static_leases": prom.Gauge("dhcp_static_leases", "Number of static DHCP leases")
+        "enabled": prom.Gauge("dhcp_enabled", "DHCP server status",
+                              namespace=env.PROMETHEUS_PREFIX),
+        "active_leases": prom.Gauge("dhcp_active_leases", "Number of active DHCP leases",
+                                    namespace=env.PROMETHEUS_PREFIX),
+        "static_leases": prom.Gauge("dhcp_static_leases", "Number of static DHCP leases",
+                                    namespace=env.PROMETHEUS_PREFIX)
     },
     "filtering": {
-        "enabled": prom.Gauge("filtering_enabled", "Filtering status"),
-        "active_lists": prom.Gauge("filtering_active_lists", "Number of active filtering lists"),
-        "total_filters": prom.Gauge("filtering_total_filters", "Number of filtering rules"),
-        "user_rules": prom.Gauge("filtering_user_rules", "Number of user-defined filtering rules")
+        "enabled": prom.Gauge("filtering_enabled", "Filtering status",
+                              namespace=env.PROMETHEUS_PREFIX),
+        "active_lists": prom.Gauge("filtering_active_lists", "Number of active filtering lists",
+                                   namespace=env.PROMETHEUS_PREFIX),
+        "total_filters": prom.Gauge("filtering_total_filters", "Number of filtering rules",
+                                    namespace=env.PROMETHEUS_PREFIX),
+        "user_rules": prom.Gauge("filtering_user_rules", "Number of user-defined filtering rules",
+                                 namespace=env.PROMETHEUS_PREFIX)
     },
     "status": {
-        "running": prom.Gauge("status_running", "Server status"),
-        "protection_enabled": prom.Gauge("status_protection_enabled", "Protection status")
+        "running": prom.Gauge("status_running", "Server status",
+                              namespace=env.PROMETHEUS_PREFIX),
+        "protection_enabled": prom.Gauge("status_protection_enabled", "Protection status",
+                                         namespace=env.PROMETHEUS_PREFIX)
     },
     "rewrite": {
-        "rules": prom.Gauge("rewrite_rules", "Number of DNS rewrite rules")
+        "rules": prom.Gauge("rewrite_rules", "Number of DNS rewrite rules",
+                            namespace=env.PROMETHEUS_PREFIX)
     },
     "dns": {
-        "num_queries": prom.Gauge("dns_num_queries", "Number of DNS queries"),
-        "num_blocked": prom.Gauge("dns_num_blocked", "Number of blocked DNS queries"),
-        "num_replaced": prom.Gauge("dns_num_replaced", "Number of replaced DNS queries"),
-        "avg_processing_time": prom.Gauge("dns_avg_processing_time", "Average DNS query processing time")
+        "num_queries": prom.Gauge("dns_num_queries", "Number of DNS queries",
+                                  namespace=env.PROMETHEUS_PREFIX),
+        "num_blocked": prom.Gauge("dns_num_blocked", "Number of blocked DNS queries",
+                                  namespace=env.PROMETHEUS_PREFIX),
+        "num_replaced": prom.Gauge("dns_num_replaced", "Number of replaced DNS queries",
+                                   namespace=env.PROMETHEUS_PREFIX),
+        "avg_processing_time": prom.Gauge("dns_avg_processing_time", "Average DNS query processing time",
+                                          namespace=env.PROMETHEUS_PREFIX)
     },
     "stats": {
-        "top_domains": prom.Gauge("stats_top_domains", "Top queried domains", ["domain"]),
-        "top_clients": prom.Gauge("stats_top_clients", "Top DNS clients", ["ip", "name"]),
-        "top_blocked_domains": prom.Gauge("stats_top_blocked_domains", "Top blocked domains", ["domain"]),
-        "top_upstreams_responses": prom.Gauge("stats_top_upstreams_responses", "Top upstream responses", ["upstream"]),
-        "top_upstreams_avg_time": prom.Gauge("stats_top_upstreams_avg_time", "Top upstream average response time", ["upstream"])
+        "top_domains": prom.Gauge("stats_top_domains", "Top queried domains",
+                                  ["domain"], namespace=env.PROMETHEUS_PREFIX),
+        "top_clients": prom.Gauge("stats_top_clients", "Top DNS clients",
+                                  ["ip", "name"], namespace=env.PROMETHEUS_PREFIX),
+        "top_blocked_domains": prom.Gauge("stats_top_blocked_domains", "Top blocked domains",
+                                          ["domain"], namespace=env.PROMETHEUS_PREFIX),
+        "top_upstreams_responses": prom.Gauge("stats_top_upstreams_responses", "Top upstream responses",
+                                              ["upstream"], namespace=env.PROMETHEUS_PREFIX),
+        "top_upstreams_avg_time": prom.Gauge("stats_top_upstreams_avg_time", "Top upstream average response time",
+                                             ["upstream"], namespace=env.PROMETHEUS_PREFIX)
     }
 }
 
